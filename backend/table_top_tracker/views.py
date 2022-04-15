2bb4e1fe8111362b94f0851e9ac24b54a60fe1a9
Chowon Jung <56695129+dancing4am@users.noreply.github.com>
2022-04-15 17:24:05 -0400

    more view methods added

--- backend/table_top_tracker/views.py
+++ backend/table_top_tracker/views.py
@@ -21,7 +21,7 @@ class all_games(View):
 
 class all_users(View):
     def get(self, request):
-        users = User.objects
+        users = User.objects#.order_by('name')
         names = users.name.all()
         context = {
             'names' : names
@@ -43,7 +43,7 @@ class all_game_objects(View):
 
 class all_game_event(View):
     def get(self, request):
-        gameEvents = GameEvent.objects
+        gameEvents = GameEvent.objects#.order_by('date')
         dates = gameEvents.game_date.all()
         times = gameEvents.game_time.all()
         players = gameEvents.players.all()
@@ -57,22 +57,23 @@ class all_game_event(View):
         }
         return render(request, "gameEvent/gameEvent_list.html", context) # temporary html file name
 
+# single game event by event id
 class game_event(View):
     def get(self, request):
-        gameEvents = GameEvent.objects
-        gameEvent = gameEvents.filter(id=request.event_id)
-        name = gameEvent.game.name
-        date = gameEvent.game_date
-        time = gameEvent.game_time
-        players = gameEvent.players
-        context = {
-            'name' : name,
-            'date' : date,
-            'time' : time,
-            'players' : players
-        }
-        return render(request, 'gameEvent/{}/gameEvent_info.html'.format(request.event_id), context) # temporary html file name
-
+        if request.method == 'GET':
+            gameEvents = GameEvent.objects
+            gameEvent = gameEvents.filter(id=request.event_id)
+            name = gameEvent.game.name
+            date = gameEvent.game_date
+            time = gameEvent.game_time
+            players = gameEvent.players
+            context = {
+                'name' : name,
+                'date' : date,
+                'time' : time,
+                'players' : players
+            }
+            return render(request, 'gameEvent/{}/gameEvent_info.html'.format(request.event_id), context) # temporary html file name
 
 class game_data(View):
     def get(self, request):
@@ -81,16 +82,22 @@ class game_data(View):
 class user_game_data(View):
 # @login_required
     def get(self, request):
-        gameData = request.user.game_list  
-        names = gameData.name.all()
-        game_objects = gameData.game_objects.all()
-        times_played = gameData.times_played.all()
-        context={
-            'names' : names,
-            'game_objects' : game_objects,
-            'times_played' : times_played
-        }
-        return render(request, 'user/{}/game'.format(request.user.id), context)
+        if request.method == 'GET':
+            gameData = request.user.game_list  
+            names = gameData.name.all()
+            game_objects = gameData.game_objects.all()
+            times_played = gameData.times_played.all()
+            context={
+                'names' : names,
+                'game_objects' : game_objects,
+                'times_played' : times_played
+            }
+            return render(request, 'user/{}/game'.format(request.user.id), context)
+
+    def post(self, request):
+        if request.method == 'POST':
+            user = request.user
+#            user.game_list.add()
 
 class game_game_objects(View):
     def get(self, request):
@@ -106,17 +113,32 @@ class game_game_objects(View):
         }
         return render(request, 'game/{}/gameObjects/gameObject.html'.format(request.game_id), context)
 
+# game event by game id
 class game_game_event(View):
     def get(self, request):
-        
-        pass
+        gameEvents = GameEvent.objects
+        gameEvent = gameEvents.filter(game.game_id=request.game_id)
+        name = gameEvent.game.name
+        date = gameEvent.game_date
+        time = gameEvent.game_time
+        players = gameEvent.players
+        context = {
+            'name' : name,
+            'date' : date,
+            'time' : time,
+            'players' : players
+        }
+        return render(request, 'gameEvent/{}/gameEvent_info.html'.format(request.game_id), context) # temporary html file name
+
 
 class game_stats(View):
     def get(self, request):
         pass 
 
 class user_stats(View):
+# @login_required
     def get(self, request):
-        stats = request.user
-
+        user = User.objects.filter(id=request.user_id)
+        name = user.name
+        game_list = user.game_list
         pass
